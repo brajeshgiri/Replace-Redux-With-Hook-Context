ea68feb64f73d778a54dc5177e3fce2fed588277
Brajesh Goswami <goswami48@gmail.com>
2020-02-15 03:28:44 +0530

    add few test cases 2

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,6 @@ import App from './App';
 import { create } from "react-test-renderer";
 
 describe('App Component', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-  });
 
   it("Matches the snapshot", () => {
     const app = create(<App />);
