deeae0623df2f98f9467269c8978b6e64a5024ec
Brajesh Goswami <goswami48@gmail.com>
2020-02-15 03:18:53 +0530

    add few test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { create } from "react-test-renderer";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App Component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  it("Matches the snapshot", () => {
+    const app = create(<App />);
+    expect(app.toJSON()).toMatchSnapshot();
+  });
 });
+
