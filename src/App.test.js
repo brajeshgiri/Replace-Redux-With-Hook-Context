da2ab587ab951ba924050de9230f0a478056cd67
Brajesh Goswami <goswami48@gmail.com>
2020-03-04 10:53:07 +0530

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { create } from "react-test-renderer";
-
 describe('App Component', () => {
 
   it("Matches the snapshot", () => {
